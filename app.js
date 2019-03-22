c921d2577625a3fefbd96eef58a9e1a74a53698a
wxh_leo <754557634@qq.com>
2019-03-22 16:25:57 +0800

    初始化项目

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const Hapi = require('hapi');
 require('env2')('./.env');
 const config = require('./config');
+const hello = require('./routes/hello');
+const pluginHapiSwagger = require('./plugins/hapi-swagger');
 
 const server = new Hapi.Server();
 // 配置服务器启动的 host 和端口
@@ -9,6 +11,13 @@ server.connection({
   port: config.port
 })
 const init = async () => {
+  await server.register([
+    // 为系统使用 hapi-swagger
+    ...pluginHapiSwagger,
+  ]);
+  server.route([
+    ...hello,
+  ])
   await server.start();
   console.log(`Server running at: ${server.info.uri}`);
 }
